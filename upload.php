db30b3b454b620912b270c83e94aa4de48f2e13e
root <root@Gentoo-Server.sd.sd.cox.net>
2016-11-12 21:21:39 -0800

    Added database functionality to upload.php

---
+++ upload.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$success = "";
+include_once($_SERVER['DOCUMENT_ROOT'] . "/settings.php");
+if (empty ( $_SESSION ['id'] )) {
+	header ( "Location: " . $_SERVER['DOCUMENT_ROOT'] . "/login.php" );
+} else {
+	if (isset ( $_FILES ['file'] )) {
+		$file = $_FILES ['file'];
+		$file_name = $file ['name'];
+		$file_tmp = $file ['tmp_name'];
+		$file_size = $file ['size'];
+		$file_error = $file ['error'];
+		$file_ext = explode ( '.', $file_name );
+		$file_ext = strtolower ( end ( $file_ext ) );
+		if (in_array ( $file_ext, $allowed ) && $file_error == 0 && $file_size <= $max_filesize) {
+			$file_destination = $absolute_path . time() . '.' . $file_ext;
+			if (move_uploaded_file ( $file_tmp, $file_destination )) {
+				mysqli_query($databaseConnect, "INSERT INTO images (path, name) VALUES ('$file_destination', '$file_name')");
+				$success = "true";
+			}
+		}
+	}
+}
+ 	?>
