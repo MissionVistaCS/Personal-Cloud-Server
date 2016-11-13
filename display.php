4cf99fa3c3960258c18cf36e040a03351710ad75
Ryan Keegan <r.keegan@me.com>
2016-11-13 08:37:15 -0800

    Adapted display.php to Android app

--- display.php
+++ display.php
@@ -4,9 +4,11 @@
     if (empty ( $_SESSION ['id'] )) {
         header ( "Location: " . $_SERVER['DOCUMENT_ROOT'] . "/login.php" );
     } else {
-        $file = basename(urldecode($_GET['file']));
-        if (file_exists($absolute_path . $file)) {
-            $contents = file_get_contents($absolute_path . $file);
+        $timestamp = basename(urldecode($_GET['timestamp']));
+        $query = mysqli_query($databaseConnect, "SELECT path FROM images WHERE timestamp='$timestamp'");
+        $row = mysqli_fetch_row($query);
+        if (file_exists($row['0'])) {
+            $contents = file_get_contents($row['0']);
             header('Content-type: image/jpeg');
             echo $contents;
         }
